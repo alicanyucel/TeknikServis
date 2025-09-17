63855446565f1d497037207a357b3f7bc2707213
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-09-17 15:13:19 +0300

    basedto

---
+++ TeknikServis/TeknikServis.Application/Dtos/BaseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TeknikServis.Application.Dtos;
+
+public abstract record BaseDto(
+    TimeOnly UpdatedTime,
+    TimeOnly CreatedTime,
+    string UpdatedBy,
+    string CreatedBy,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt,
+    bool IsDeleted
+);
