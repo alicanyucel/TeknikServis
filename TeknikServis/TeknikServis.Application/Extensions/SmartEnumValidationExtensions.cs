324a62ed9235eb08cca7c08ad2f1e592a7b71c43
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-09-17 22:02:53 +0300

    Extensions Metot

---
+++ TeknikServis/TeknikServis.Application/Extensions/SmartEnumValidationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.SmartEnum;
+using FluentValidation;
+
+namespace TeknikServis.Application.Extensions;
+
+public static class SmartEnumValidationExtensions
+{
+    public static IRuleBuilderOptions<T, TProperty> IsValidSmartEnum<T, TProperty>(
+        this IRuleBuilder<T, TProperty> ruleBuilder)
+        where TProperty : SmartEnum<TProperty>
+    {
+        return ruleBuilder.Must(value => value != null && SmartEnum<TProperty>.List.Contains(value))
+            .WithMessage("'{PropertyName}' geçersiz bir değer.");
+    }
+
+    public static IRuleBuilderOptions<T, int> IsValidSmartEnumValue<T, TSmartEnum>(
+        this IRuleBuilder<T, int> ruleBuilder)
+        where TSmartEnum : SmartEnum<TSmartEnum>
+    {
+        return ruleBuilder.Must(value => SmartEnum<TSmartEnum>.List.Any(x => x.Value == value))
+            .WithMessage($"'{{PropertyName}}' geçersiz bir {typeof(TSmartEnum).Name} değeri.");
+    }
+
+    public static IRuleBuilderOptions<T, int?> IsValidSmartEnumValue<T, TSmartEnum>(
+        this IRuleBuilder<T, int?> ruleBuilder)
+        where TSmartEnum : SmartEnum<TSmartEnum>
+    {
+        return ruleBuilder.Must(value => value == null || SmartEnum<TSmartEnum>.List.Any(x => x.Value == value))
+            .WithMessage($"'{{PropertyName}}' geçersiz bir {typeof(TSmartEnum).Name} değeri.");
+    }
+}
