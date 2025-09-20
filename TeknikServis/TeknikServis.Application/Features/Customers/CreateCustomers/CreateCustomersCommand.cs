4d2dcb59f527644572335d979334499a81a9711f
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-09-20 11:08:23 +0300

    OK

--- TeknikServis/TeknikServis.Application/Features/Customers/CreateCustomers/CreateCustomersCommand.cs
+++ TeknikServis/TeknikServis.Application/Features/Customers/CreateCustomers/CreateCustomersCommand.cs
@@ -4,15 +4,23 @@ using TS.Result;
 namespace TeknikServis.Application.Features.Customers.CreateCustomers;
 
 public sealed record CreateCustomerCommand(
-string Name,
-string Surname,
-string PhoneNumber,
-string Email,
-string Address,
-string City,
-string Country,
-string ZipCode,
-string District,
-string Neighborhood,
-int CustomerValue
-) : IRequest<Result<string>>;
+    string Name,
+    string Surname,
+    string PhoneNumber,
+    string Email,
+    string Address,
+    string City,
+    string Country,
+    string ZipCode,
+    string District,
+    string Neighborhood,
+    int CustomerValue,
+    TimeOnly UpdatedTime,
+    TimeOnly CreatedTime,
+    string? UpdatedBy,
+    string CreatedBy,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt,
+    bool IsDeleted
+) :IRequest<Result<string>>;
+
--- TeknikServis/TeknikServis.Application/Mapping/MappingClass.cs
+++ TeknikServis/TeknikServis.Application/Mapping/MappingClass.cs
@@ -10,13 +10,7 @@ public sealed class MappingClass : Profile
     public MappingClass()
     {
         CreateMap<CreateCustomerCommand, Customer>()
-            .ForMember(dest => dest.CustomerType, opt => opt.MapFrom(src => CustomerType.FromValue(src.CustomerValue)))
-            .ForMember(dest => dest.CreatedTime, opt => opt.MapFrom(src => TimeOnly.FromDateTime(DateTime.Now)))
-            .ForMember(dest => dest.UpdatedTime, opt => opt.MapFrom(src => TimeOnly.FromDateTime(DateTime.Now)))
-            .ForMember(dest => dest.CreateadAt, opt => opt.MapFrom(src => DateTime.Now))
-            .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => "System"))
-            .ForMember(dest => dest.UpdatedBy, opt => opt.MapFrom(src => "System"))
-            .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => false));
+            .ForMember(dest => dest.CustomerType, opt => opt.MapFrom(src => CustomerType.FromValue(src.CustomerValue)));
 
         CreateMap<Customer, CustomerDto>();
     }
