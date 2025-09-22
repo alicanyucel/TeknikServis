9893c6d6d5539a4832ecfd36dd67a99c1df90e8b
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2025-09-22 12:27:49 +0300

    Delete CreateDocumentLinkCommand.cs

--- CreateDocumentLinkCommand.cs
+++
@@ -1,8 +0,0 @@
-public sealed record CreateDocumentLinkCommand : IRequest<Result<string>>, IBaseRequest, IEquatable<CreateDocumentLinkCommand>
-{
-    public string Url { get; init; }
-    public string Description { get; init; }
-    public Guid ServiceActionId { get; init; }
-    public string CreatedBy { get; init; } 
-    public string UpdatedBy { get; init; } 
-}
