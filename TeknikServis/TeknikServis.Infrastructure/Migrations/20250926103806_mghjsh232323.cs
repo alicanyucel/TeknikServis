db0092f1eeed2e45499931aee680edfad8e92014
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-09-26 14:34:07 +0300

    ..kk

---
+++ TeknikServis/TeknikServis.Infrastructure/Migrations/20250926103806_mghjsh232323.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeknikServis.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mghjsh232323 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "CratedTime",
+                table: "AppUsers",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateadAt",
+                table: "AppUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "AppUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AppUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AppUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdatedBy",
+                table: "AppUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "UpdatedTime",
+                table: "AppUsers",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CratedTime",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreateadAt",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedBy",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedTime",
+                table: "AppUsers");
+        }
+    }
+}
