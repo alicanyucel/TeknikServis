4aac8482463eee12ca985ae2fb0d00b37a51fe4a
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-10-22 11:35:00 +0300

    a

--- TeknikServis/TeknikServis.Application/Constanst/Country.cs
+++ TeknikServis/TeknikServis.Application/Constanst/Country.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TeknikServis.Domain.Entities;
 
 namespace TeknikServis.Application.Constanst;
 
 public static class CountryConstants
 {
-    public static readonly IReadOnlyList<Country> Countries = new List<Country>
-{
-    new Country { Id = 179, Code = "TR", Name = "TÜRKİYE", NormalizedName = "TURKIYE" },
-    new Country { Id = 840, Code = "US", Name = "UNITED STATES", NormalizedName = "UNITED STATES" },
-    new Country { Id = 276, Code = "DE", Name = "GERMANY", NormalizedName = "GERMANY" },
-    new Country { Id = 250, Code = "FR", Name = "FRANCE", NormalizedName = "FRANCE" },
-    new Country { Id = 826, Code = "GB", Name = "UNITED KINGDOM", NormalizedName = "UNITED KINGDOM" },
-    new Country { Id = 124, Code = "CA", Name = "CANADA", NormalizedName = "CANADA" },
-    new Country { Id = 392, Code = "JP", Name = "JAPAN", NormalizedName = "JAPAN" },
-    new Country { Id = 156, Code = "CN", Name = "CHINA", NormalizedName = "CHINA" },
-    new Country { Id = 76,  Code = "BR", Name = "BRAZIL", NormalizedName = "BRAZIL" },
-    new Country { Id = 36,  Code = "AU", Name = "AUSTRALIA", NormalizedName = "AUSTRALIA" }
-};
+    public static readonly IReadOnlyList<Country> Countries = new[]
+    {
+        new Country
+        {
+            Id = 1,
+            Code = 792,
+            Nr = 90,
+            Name = "TÜRKİYE",
+            CreatedTime = TimeOnly.MinValue,
+            UpdatedTime =TimeOnly.MinValue,
+            CreatedBy = string.Empty,
+            UpdatedBy = string.Empty,
+            CreateadAt = DateTime.MinValue
+        },
+
+    };
 }
