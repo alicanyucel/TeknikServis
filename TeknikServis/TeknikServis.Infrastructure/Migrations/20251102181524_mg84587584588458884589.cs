43fc0234a81b073a533fec68b62f70ade45a4140
Ali Can Yücel <yucelalicandan@hotmail.com>
2025-11-02 21:16:50 +0300

    ..

---
+++ TeknikServis/TeknikServis.Infrastructure/Migrations/20251102181524_mg84587584588458884589.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeknikServis.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg84587584588458884589 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Customers_CustomerId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Neighbourhoods");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Districts_PostalCode",
+                table: "Districts");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Districts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Customers_CustomerId",
+                table: "Products",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Customers_CustomerId",
+                table: "Products");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Districts",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Neighbourhoods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DistrictId = table.Column<int>(type: "int", nullable: false),
+                    CreateadAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedTime = table.Column<TimeOnly>(type: "time", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedTime = table.Column<TimeOnly>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Neighbourhoods", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Neighbourhoods_Districts_DistrictId",
+                        column: x => x.DistrictId,
+                        principalTable: "Districts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Districts_PostalCode",
+                table: "Districts",
+                column: "PostalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Neighbourhoods_DistrictId",
+                table: "Neighbourhoods",
+                column: "DistrictId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Customers_CustomerId",
+                table: "Products",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
